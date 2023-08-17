cf449a1f04d6fbdeb84b1b01575a622a43e64657
freemancho1 <freeman.cho@gmail.com>
2023-08-17 19:30:51 +0900

    h2308172203

--- 60-GEOAI-GEOAIP39/geo_coding_31.py
+++ 60-GEOAI-GEOAIP39/geo_coding_31.py
@@ -26,6 +26,16 @@ def get_geocoding(address_name):
     response = requests.get(req_uri, headers=REQUEST_HEADER)
     return response.json() if response.status_code == 200 else {'status': 'error'}
 
+def get_optimal_path(start, end):
+    optimal_api_url = \
+        f'https://naveropenapi.apigw.ntruss.com/map-direction/v1/driving?' \
+        f'start={start[1]},{start[0]}&goal={end[1]},{end[0]}' \
+        f'&option=trafast:tracomfort:traoptimal&format=json'
+        #  실시간  빠른길  편한길      최적
+    print(f'{optimal_api_url}')
+    response = requests.get(optimal_api_url, headers=REQUEST_HEADER)
+    return response.json() if response.status_code == 200 else {'status': 'error'}
+
 
 if __name__ == '__main__':
     address = '전남 나주시 중야1길 15'
