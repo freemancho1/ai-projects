e7bac40e7a137c274c933881898ff5095e570977
freemancho1 <freeman.cho@gmail.com>
2023-09-09 21:48:30 +0900

    h2309092148

--- 41-AIDDD-T213P310/aiddd/data_manager.py
+++ 41-AIDDD-T213P310/aiddd/data_manager.py
@@ -2,13 +2,6 @@ import os
 import warnings
 import pandas as pd
 
-# 엑셀파일을 읽을 때, openpyxl라이브러리에서 스타일관련 오류를 출력하는데, 
-# 일반적으로 이 경고는 무시해도 무방하기 때문에 설정을 변경함
-warnings.filterwarnings(
-    'ignore',
-    category=UserWarning,
-    module='openpyxl.styles.stylesheet'
-)
 
 _PATH_BASE = os.path.join(os.path.expanduser('~'), 'projects', 'data', 'AIDDD')
 
@@ -16,7 +9,7 @@ def _get_path(*paths):
     path = [_PATH_BASE] + [_path for _path in paths]
     return os.path.join(*path)
 
-_PATH_FILES = {
+_PATH_READ_FILES = {
     'kbt 고객':     _get_path('KBT(설비)', 'KBT(설비) 추출내역', 'KBT 저압고객 추출내역.xlsx'),
     'kbt 인입선':   _get_path('KBT(설비)', 'KBT(설비) 추출내역', 'KBT 저압인입선 추출내역.xlsx'),
     'kbt 장주애자': _get_path('KBT(설비)', 'KBT(설비) 추출내역', 'KBT 저압장주애자 추출내역.xlsx'),
@@ -26,6 +19,7 @@ _PATH_FILES = {
     'kbt 지지선':   _get_path('KBT(설비)', 'KBT(설비) 추출내역', 'KBT 지지선 추출내역.xlsx'),
     # '공사비':     _get_path('공사비예산서', '5개년 공사비 예산서 추출내역.xlsx'),
     '공사비':       _get_path('공사비예산서', '5개년 공사비 예산서 추출내역_계약전력등추가.xlsx'),
+    '공사비 공사비': _get_path('공사비예산서', '5개년 공사비 예산서 추출내역_계약전력등추가.xlsx'),
     '기별 애자':    _get_path('기별', '추출내역', '계약전력등추가', '5개년 저압애자_기별(자재,품,해설_기계경비) 추출내역_계약전력등추가.xlsx'),
     '기별 인입선':  _get_path('기별', '추출내역', '계약전력등추가', '5개년 저압인입선_기별(자재,품,해설_기계경비) 추출내역_계약전력등추가.xlsx'),
     '기별 장주':    _get_path('기별', '추출내역', '계약전력등추가', '5개년 저압장주_기별(자재,품,해설_기계경비) 추출내역_계약전력등추가.xlsx'),
@@ -43,4 +37,43 @@ _PATH_FILES = {
     '설계 지지선':  _get_path('설계', '설계 추출내역', '계약전력등추가', '5개년 지지선 설계 추출내역_계약전력등추가.xlsx'),
 }
 
-get_data = lambda type: pd.read_excel(_PATH_FILES[type])
+_PATH_READ_WRITE_FILES = {
+    '통계 전체': _get_path('전처리데이터', '통계 전체.csv'),
+    '통계 1차전처리': _get_path('전처리데이터', '통계 1차전처리.csv'),
+    '통계 2차전처리': _get_path('전처리데이터', '통계 2차전처리.csv'),
+    '병합 전체카운트': _get_path('전처리데이터', '병합 전체카운트.csv'),
+    '전처리 공사비': _get_path('전처리데이터', '전처리공사비.csv'),
+    '전처리 공사비+설계': _get_path('전처리데이터', '전처리공사비_설계.csv'),
+    '전처리 공사비+설계기별': _get_path('전처리데이터', '전처리공사비_설계기별.csv'),
+    # '전처리 설계전선': _get_path('전처리데이터', '전처리 설계전선.csv'),
+    # '전처리 설계전주': _get_path('전처리데이터', '전처리 설계전주.csv'),
+}
+
+_IS_SETTING = False
+
+def read_data(type):
+    global _IS_SETTING
+    
+    if _IS_SETTING == False:
+        _IS_SETTING = True
+        # 엑셀파일을 읽을 때, openpyxl라이브러리에서 스타일관련 오류를 출력하는데, 
+        # 일반적으로 이 경고는 무시해도 무방하기 때문에 설정을 변경함
+        warnings.filterwarnings(
+            'ignore',
+            category=UserWarning,
+            module='openpyxl.styles.stylesheet'
+        )
+    
+    try:
+        file_path = _PATH_READ_FILES[type]
+    except KeyError as ke:
+        file_path = _PATH_READ_WRITE_FILES[type]
+    # 파일확장자를 비교해 읽는 함수를 결정
+    file_ext = os.path.splitext(file_path)[1]
+    return pd.read_excel(file_path) if file_ext.lower() == '.xlsx' else pd.read_csv(file_path)
+    
+# get_data = lambda type: pd.read_excel(_PATH_FILES[type])
+
+def write_data(type, df, index=False):
+    # 전처리 이후 파일은 무조건 csv파일로 관리
+    df.to_csv(_PATH_READ_WRITE_FILES[type], index=index)
