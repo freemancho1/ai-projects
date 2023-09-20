e88393417323676e5167b7d81755ae522483d946
freemancho1 <freeman.cho@gmail.com>
2023-09-20 18:02:54 +0900

    o2309201800

--- utils/freeman/aiddd/data_manager.py
+++ utils/freeman/aiddd/data_manager.py
@@ -11,20 +11,34 @@ DATA_BASE_PATH = os.path.join(
 
 # cons: 공사비, line: 전선, pole: 전주, sl: service line(인입선)
 PROVIDED_DATA_FILES = {
+    # 2차: 공사비, 전주/전선/인입주
     '2nd provide cons': ['2nd', 'provided_data', 'CONS_INFO.xlsx'],
     '2nd provide line': ['2nd', 'provided_data', 'LINE_DATA.xlsx'],
     '2nd provide pole': ['2nd', 'provided_data', 'POLE_DATA.xlsx'],
     '2nd provide sl':   ['2nd', 'provided_data', 'SL_DATA.xlsx'],
+    # 3차: 전주/전선/인입주
+    '3rd provide line': ['3rd', 'provided_data', 'LINE_DATA.xlsx'],
+    '3rd provide pole': ['3rd', 'provided_data', 'POLE_DATA.xlsx'],
+    '3rd provide sl':   ['3rd', 'provided_data', 'SL_DATA.xlsx'],
 } 
 
 PREPROCESSED_DATA_FILES = {
-    # ba-pp-cons.ipynb
+    # 2nd: ba-pp-cons.ipynb
     '2nd pp cons-1st': ['2nd', 'preprocessed_data', 'cons-1st.csv'],
     '2nd pp cons-1st-all': ['2nd', 'preprocessed_data', 'cons-1st-all.csv'],
-    # bb-pp-device-counts-base-on-cons.ipynb <- '2nd pp cons-1st'
+    # 2nd: bb-pp-device-counts-base-on-cons.ipynb <- '2nd pp cons-1st'
     '2nd pp counts-base-on-cons-1st': ['2nd', 'preprocessed_data', 'counts-base-on-cons-1st.csv'],
-    # bc-pp-pole-position-base-on-cons.ipynb <- '2nd pp counts-base-on-cons-1st'
+    # 2nd: bc-pp-pole-position-base-on-cons.ipynb <- '2nd pp counts-base-on-cons-1st'
     '2nd pp pole-position-on-cons-1st': ['2nd', 'preprocessed_data', 'pole-position-base-on-cons-1st.csv'],
+    
+    # 2nd vs 3rd: 전처리 후 컬럼명 영문으로 변경, 좌표값
+    # 3rd: ba-pp-cons.ipynb
+    '3rd pp cons-1st': ['3rd', 'preprocessed_data', 'cons-1st.csv'],
+    '3rd pp cons-1st-all': ['3rd', 'preprocessed_data', 'cons-1st-all.csv'],
+    # 3rd: bb-pp-device-counts-base-on-cons.ipynb <- '3rd pp cons-1st'
+    '3rd pp counts-base-on-cons-1st': ['3rd', 'preprocessed_data', 'counts-base-on-cons-1st.csv'],
+    # 3rd: bc-pp-pole-position-base-on-cons.ipynb <- '3rd pp counts-base-on-cons-1st'
+    '3rd pp pole-position-on-cons-1st': ['3rd', 'preprocessed_data', 'pole-position-base-on-cons-1st.csv'],
 }
 
 # DATA_FILES = PROVIDED_DATA_FILES + PREPROCESSED_DATA_FILES
